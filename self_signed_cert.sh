b1a78648f2466f6fe0e0bcac7483bf59e98eb9fd
Чуриков Антон Алексеевич <stack.anton.churikov@hotmail.com>
2025-01-24 10:16:08 +0300

    feat: Свой сертификат

--- self_signed_cert.sh
+++ self_signed_cert.sh
@@ -10,14 +10,6 @@ if ! command -v openssl &> /dev/null; then
   fi
 fi
 
-# Установка qrencode
-if ! command -v qrencode &> /dev/null; then
-  sudo apt update && sudo apt install -y qrencode
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
-fi
-
 # Функция ожидания нажатия Enter
 wait_for_enter() {
   echo -e "Нажмите Enter, чтобы продолжить..."
@@ -43,46 +35,6 @@ else
   x-ui
 fi
 
-# ASCII-арт
-cat << "EOF"
-============================================================
-       ПОДПИШИСЬ НА НАС НА YOUTUBE: ANTEN-KA
-============================================================
-EOF
-
-# QR-код для чаевых
-echo "############################################################"
-echo "#                    QR-КОД ДЛЯ ЧАЕВЫХ                     #"
-echo "############################################################"
-TIP_LINK="https://pay.cloudtips.ru/p/7410814f"
-qrencode -t ANSIUTF8 "$TIP_LINK"
-wait_for_enter
-
-# Разделитель из 3 строк
-for i in {1..3}; do echo "============================================================"; done
-
-# QR-код YouTube
-echo "############################################################"
-echo "#                      QR-КОД YOUTUBE                      #"
-echo "############################################################"
-YT_LINK="https://www.youtube.com/antenkaru"
-qrencode -t ANSIUTF8 "$YT_LINK"
-wait_for_enter
-
-# Разделитель из 3 строк
-for i in {1..3}; do echo "============================================================"; done
-
-# QR-код Boosty
-echo "############################################################"
-echo "#                      QR-КОД BOOSTY                       #"
-echo "############################################################"
-BOOSTY_LINK="https://boosty.to/anten-ka"
-qrencode -t ANSIUTF8 "$BOOSTY_LINK"
-wait_for_enter
-
-# Разделитель из 3 строк
-for i in {1..3}; do echo "============================================================"; done
-
 # Генерация сертификата
 CERT_DIR="/etc/ssl/self_signed_cert"
 CERT_NAME="self_signed"
@@ -94,7 +46,7 @@ KEY_PATH="$CERT_DIR/$CERT_NAME.key"
 openssl req -x509 -nodes -days $DAYS_VALID -newkey rsa:2048 \
   -keyout "$KEY_PATH" \
   -out "$CERT_PATH" \
-  -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=example.com"
+  -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=vpn.sk-serv.ru"
 
 if [ $? -eq 0 ]; then
   echo "SSL CERTIFICATE PATH: $CERT_PATH"
